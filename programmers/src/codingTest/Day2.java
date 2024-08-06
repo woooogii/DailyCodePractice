cd00c6d98ff056460fe9f231c88e09ad2af1814a
woooogii <wldms9311@naver.com>
2024-08-06 17:39:10 +0900

    codingTest - Day2

---
+++ programmers/src/codingTest/Day2.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+public class Day2 {
+    public double solution(int[] arr){
+        int sum = 0;
+        double answer = 0;
+        for(int i=0;i<arr.length;i++){
+            sum +=arr[i];
+        }
+        answer = (double) sum / arr.length;
+        return answer;
+    }
+}

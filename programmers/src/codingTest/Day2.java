59e583ce08474acd9710d5adcc76c33609747000
woooogii <wldms9311@naver.com>
2024-08-06 17:41:27 +0900

    DOCS: Day1, Day2 주석 수정

--- programmers/src/codingTest/Day2.java
+++ programmers/src/codingTest/Day2.java
@@ -1,5 +1,11 @@
 package codingTest;
 
+/*
+정수를 담고 있는 배열 arr의 평균값을 반환
+arr은 길이 1 이상, 100 이하인 배열
+arr의 원소는 10,000 이상 10,000 이하인 정수
+ */
+
 public class Day2 {
     public double solution(int[] arr){
         int sum = 0;
